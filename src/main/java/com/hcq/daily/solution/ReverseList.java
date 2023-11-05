d16f5a9b2721e04bcbb6c6055cc07888cffcf0be
huangchengqian <huangchengqian@139.com>
2023-11-05 13:20:02 +0800

    11.05

---
+++ src/main/java/com/hcq/daily/solution/ReverseList.java
@@ -0,0 +1,11 @@
+package com.hcq.daily.solution;
+
+import com.hcq.daily.structure.ListNode;
+
+public class ReverseList {
+
+    public ListNode reverseList(ListNode head) {
+        return null;
+    }
+
+}
