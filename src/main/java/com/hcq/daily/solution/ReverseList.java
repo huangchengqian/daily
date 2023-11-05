07f78fa1e3ba92525338971508b7571b621cb387
huangchengqian <huangchengqian@139.com>
2023-11-05 13:35:22 +0800

    反转链表

--- src/main/java/com/hcq/daily/solution/ReverseList.java
+++ src/main/java/com/hcq/daily/solution/ReverseList.java
@@ -4,8 +4,21 @@ import com.hcq.daily.structure.ListNode;
 
 public class ReverseList {
 
+    /**
+     * 反转链表
+     * @param head 头
+     * @return 反转后的值
+     */
     public ListNode reverseList(ListNode head) {
-        return null;
+        ListNode pre = null;
+        ListNode node = head;
+        while (head != null) {
+            head = head.next;
+            node.next = pre;
+            pre = node;
+            node = head;
+        }
+        return pre;
     }
 
 }
